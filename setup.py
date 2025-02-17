95dbd4d5d1590c51274b1d59618ea83983ebed33
IconikoUlG <ultroghastyt@gmail.com>
2025-02-17 18:56:35 +0100

    Setup.py updated

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
+import os
+
+def get_version():
+    return os.getenv("GITHUB_REF", "refs/tags/1.0.0").split("/")[-1]
 
 setup(
     name = "beetmon",
-    version = "1.0.0",
+    version = get_version(),
     packages = find_packages(),
     install_requires = ["watchdog","beet","pyyaml"],
     entry_points = {
@@ -14,7 +18,7 @@ setup(
     description="A file watcher that runs 'beet build' on file changes.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/beetmon",
+    url="https://github.com/IconikoUlG/beetmon",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
